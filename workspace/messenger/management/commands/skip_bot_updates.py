d9d3190927ca67f75d264bb8f4c122be997d6681
Your Name <you@example.com>
2024-07-20 09:33:25 -0400

    set correct way to drop updates

--- workspace/messenger/management/commands/skip_bot_updates.py
+++ workspace/messenger/management/commands/skip_bot_updates.py
@@ -3,16 +3,26 @@ from django.conf import settings
 import telebot
 import requests
 
+from main.workers import webhook_tunneler
+
 
 class Command(BaseCommand):
     help = 'Clears all pending updates for the Telegram bot'
 
     def handle(self, *args, **kwargs):
         bot = telebot.TeleBot(settings.TELEBOT_KEY, parse_mode=None)
+        # bot.remove_webhook(drop_pending_updates=True)
+        bot.delete_webhook(drop_pending_updates=True)
 
-        bot.remove_webhook()
-        
-        url = f"https://api.telegram.org/bot{settings.TELEBOT_KEY}/getUpdates?offset=-1"
-        requests.get(url)
+        # Set webhook back
+        webhook_url = self._get_webhook_url()
+        bot.set_webhook(url=webhook_url) 
+
         self.stdout.write(self.style.SUCCESS('Successfully cleared all bot updates.'))
+
+    def _get_webhook_url(self) -> str:
+        webhook_host = settings.TELEBOT_WEBHOOK["host"]
+        webhooker = webhook_tunneler.DomainTelegramWebhooker(webhook_host)
+        webhook_url = webhooker.get_webhook_url()
+        return webhook_url
 
